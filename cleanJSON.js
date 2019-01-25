5b4d8c4e726fa836d20b3d3f45b1a6dc40f1de29
HongDucTa <43553937+HongDucTa@users.noreply.github.com>
2019-01-25 18:47:54 +0100

    removed JSON format to ease Insert commands

--- cleanJSON.js
+++ cleanJSON.js
@@ -4,13 +4,12 @@ var uuidv1 = require('uuid/v4');
 var doc = fs.readFileSync('./DBLP_clean.json');
 var rows = String(doc).split('\n');
 
-var newDoc = '[' + '\n';
+var newDoc = '';
 
-for (var i=0;i<rows.length - 2;i++)
+for (var i=0;i<rows.length - 1;i++)
 {
-    rows[i] = rows[i].replace('"_id"','"id" : "' + uuidv1() + '", "path" ');
-    newDoc = newDoc + ' ' + rows[i] + ',\n';
+    rows[i] = rows[i].replace('"_id"','"id" : "' + uuidv1() + '", "path"');
+    newDoc = newDoc + rows[i];
 }
-newDoc = newDoc + ' ' + rows[rows.length - 2] + '\n' + ']';
 
 fs.writeFileSync('./newDBLP.json',newDoc);
